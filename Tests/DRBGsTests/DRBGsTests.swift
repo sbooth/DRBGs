b47b43a6d47fe0816c62f2d5e3ae8e4bef6649a4
Stephen Booth <me@sbooth.org>
2024-06-04 07:28:14 -0500

    Constrain rotation to `UnsignedInteger` (#17)

--- Tests/DRBGsTests/DRBGsTests.swift
+++ Tests/DRBGsTests/DRBGsTests.swift
@@ -2,11 +2,31 @@ import XCTest
 @testable import DRBGs
 
 final class DRBGsTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+	func testRotate() throws {
+		let u: UInt32 = 0xABCDEF
+		for shift in 1 ..< u.bitWidth {
+			XCTAssertEqual(u.rotatedLeft(by: shift), u.rotatedRight(by: u.bitWidth - shift))
+			XCTAssertEqual(u.rotatedRight(by: shift), u.rotatedLeft(by: u.bitWidth - shift))
+		}
+	}
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
+	func testRotateLeft() throws {
+		var u: UInt32 = 0xABCD
+		u.rotateLeft(by: 16)
+		XCTAssertEqual(u, 0xABCD0000)
+		u.rotateLeft(by: 16)
+		XCTAssertEqual(u, 0xABCD)
+		u.rotateLeft(by: 3)
+		XCTAssertEqual(u, 0x55E68)
+	}
+
+	func testRotateRight() throws {
+		var u: UInt32 = 0xABCD
+		u.rotateRight(by: 16)
+		XCTAssertEqual(u, 0xABCD0000)
+		u.rotateRight(by: 16)
+		XCTAssertEqual(u, 0xABCD)
+		u.rotateRight(by: 3)
+		XCTAssertEqual(u, 0xA0001579)
+	}
 }
