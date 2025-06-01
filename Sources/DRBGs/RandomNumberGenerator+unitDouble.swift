b4b44ff9eb6a76293cc85d23b39af3151761fd96
Stephen Booth <me@sbooth.org>
2025-06-01 07:47:26 -0500

    Add inlinable to `unitDouble()` (#23)

--- Sources/DRBGs/RandomNumberGenerator+unitDouble.swift
+++ Sources/DRBGs/RandomNumberGenerator+unitDouble.swift
@@ -1,5 +1,5 @@
 //
-// Copyright © 2016-2024 Stephen F. Booth <me@sbooth.org>
+// Copyright © 2016-2025 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/DRBGs
 // MIT license
 //
@@ -9,7 +9,7 @@ extension RandomNumberGenerator {
 	/// Generates a floating-point number in the interval [0, 1)
 	///
 	/// - returns: A floating-point number *f* such that 0 ≤ *f* < 1
-	public mutating func unitDouble() -> Double {
+	@inlinable public mutating func unitDouble() -> Double {
 		let x = next()
 		return Double((x >> 11)) * 0x1.0p-53
 	}
