06bb8ff929057422e3cfa636eaade0ab015f1074
Stephen F. Booth <me@sbooth.org>
2025-06-01 07:26:54 -0500

    Add inlinable to rotation functions

--- Sources/DRBGs/FixedWidthInteger+Rotation.swift
+++ Sources/DRBGs/FixedWidthInteger+Rotation.swift
@@ -1,5 +1,5 @@
 //
-// Copyright © 2024 Stephen F. Booth <me@sbooth.org>
+// Copyright © 2024-2025 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/DRBGs
 // MIT license
 //
@@ -12,7 +12,7 @@ extension FixedWidthInteger where Self: UnsignedInteger {
 	/// - parameter shift: The length of the rotation
 	///
 	/// - returns: The left bitwise rotation of `self` by `shift`
-	func rotatedLeft(by shift: Int) -> Self {
+	@inlinable func rotatedLeft(by shift: Int) -> Self {
 		(self << shift) | (self >> (Self.bitWidth - shift))
 	}
 
@@ -23,7 +23,7 @@ extension FixedWidthInteger where Self: UnsignedInteger {
 	/// - parameter shift: The length of the rotation
 	///
 	/// - returns: The left bitwise rotation of `self` by `shift`
-	mutating func rotateLeft(by shift: Int) {
+	@inlinable mutating func rotateLeft(by shift: Int) {
 		self = self.rotatedLeft(by: shift)
 	}
 }
@@ -36,7 +36,7 @@ extension FixedWidthInteger where Self: UnsignedInteger {
 	/// - parameter shift: The length of the rotation
 	///
 	/// - returns: The right bitwise rotation of `self` by `shift`
-	func rotatedRight(by shift: Int) -> Self {
+	@inlinable func rotatedRight(by shift: Int) -> Self {
 		(self >> shift) | (self << (Self.bitWidth - shift))
 	}
 
@@ -47,7 +47,7 @@ extension FixedWidthInteger where Self: UnsignedInteger {
 	/// - parameter shift: The length of the rotation
 	///
 	/// - returns: The right bitwise rotation of `self` by `shift`
-	mutating func rotateRight(by shift: Int) {
+	@inlinable mutating func rotateRight(by shift: Int) {
 		self = self.rotatedRight(by: shift)
 	}
 }
