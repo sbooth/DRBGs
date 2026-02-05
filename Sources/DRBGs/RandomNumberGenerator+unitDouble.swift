048043023c91990270a6b3a49df29d5a805efcd6
Stephen Booth <me@sbooth.org>
2026-02-05 11:57:05 -0600

    Add SPDX file headers

--- Sources/DRBGs/RandomNumberGenerator+unitDouble.swift
+++ Sources/DRBGs/RandomNumberGenerator+unitDouble.swift
@@ -1,7 +1,8 @@
 //
-// Copyright © 2016-2025 Stephen F. Booth <me@sbooth.org>
+// SPDX-FileCopyrightText: 2016 Stephen F. Booth <contact@sbooth.dev>
+// SPDX-License-Identifier: MIT
+//
 // Part of https://github.com/sbooth/DRBGs
-// MIT license
 //
 
 // The code for unitDouble() was taken from https://prng.di.unimi.it
@@ -12,5 +13,15 @@ extension RandomNumberGenerator {
 	@inlinable public mutating func unitDouble() -> Double {
 		let x = next()
 		return Double(x >> 11) * 0x1.0p-53
+	}
+}
+
+extension RandomNumberGenerator {
+	/// Generates a floating-point number in the interval [0, 1)
+	///
+	/// - returns: A floating-point number *f* such that 0 ≤ *f* < 1
+	@inlinable public mutating func unitFloat() -> Float {
+		let x = next()
+		return Float(x >> 40) * 0x1.0p-24
 	}
 }
--- Sources/DRBGs/Xoshiro512PlusPlus.swift
+++ Sources/DRBGs/Xoshiro512PlusPlus.swift
@@ -1,7 +1,8 @@
 //
-// Copyright © 2016-2024 Stephen F. Booth <me@sbooth.org>
+// SPDX-FileCopyrightText: 2016 Stephen F. Booth <contact@sbooth.dev>
+// SPDX-License-Identifier: MIT
+//
 // Part of https://github.com/sbooth/DRBGs
-// MIT license
 //
 
 #if canImport(Darwin)
