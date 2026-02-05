6c2c133b989bc472f2e2d51ddf55fe9a0b753c41
Stephen Booth <me@sbooth.org>
2026-02-05 12:07:09 -0600

    Remove unitFloat

--- Sources/DRBGs/RandomNumberGenerator+unitDouble.swift
+++ Sources/DRBGs/RandomNumberGenerator+unitDouble.swift
@@ -15,13 +15,3 @@ extension RandomNumberGenerator {
 		return Double(x >> 11) * 0x1.0p-53
 	}
 }
-
-extension RandomNumberGenerator {
-	/// Generates a floating-point number in the interval [0, 1)
-	///
-	/// - returns: A floating-point number *f* such that 0 ≤ *f* < 1
-	@inlinable public mutating func unitFloat() -> Float {
-		let x = next()
-		return Float(x >> 40) * 0x1.0p-24
-	}
-}
